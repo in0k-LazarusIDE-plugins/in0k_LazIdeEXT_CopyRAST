759517a6cccf47a4d61be6993dd06cb0a8026ddd
in0k <slvr9@mail.ru>
2016-09-14 16:49:44 +0500

    оно заменяется!

--- srcPKG/OperationNODEs/lazExt_CopyRAST_operation_PSF_updateUsesMain.pas
+++ srcPKG/OperationNODEs/lazExt_CopyRAST_operation_PSF_updateUsesMain.pas
@@ -14,7 +14,7 @@ interface
 uses {$ifDef in0k_lazExt_CopyRAST_wndCORE___DebugLOG}in0k_lazIdeSRC_DEBUG,{$endIf}
   lazExt_CopyRAST_node,  lazExt_CopyRAST_node_ROOT,
   lazExt_CopyRAST_node_File,
-  FileUtil, LazFileUtils, CodeTree, LinkScanner,
+  FileUtil, LazFileUtils, CodeTree, LinkScanner,  SourceChanger, CodeAtom,
   CodeToolManager, CodeCache, StdCodeTools{CodeTree};
 
 type
@@ -77,8 +77,11 @@ var Code:TCodeBuffer;
     UsesNode:TCodeTreeNode;
 
     fndNAME:string;
-
+    needSave:integer;
 begin
+    needSave:=0;
+
+
    _mssge_:=Node.Get_Target_fullName;
     result:=FileExistsUTF8(Node.Get_Target_fullName);
     if result then begin
@@ -93,86 +96,44 @@ begin
             if not result then _mssge_:='CodeTool:"'+_mssge_+'" NOT received'
         end;
         //---
+
+        // Step 2: connect the SourceChangeCache
+        CodeToolBoss.SourceChangeCache.MainScanner:=Tool.Scanner;
+
+        // Step 3: EDIT
         if result then begin
             fndNAME:=upcase(ExtractFileNameWithoutExt(node.Get_Source_obj_Name));
-
-
-
             tool.MoveCursorToCleanPos(0);
             while tool.CurPos.EndPos<tool.SrcLen do begin
-                if tool.UpAtomIs(fndNAME) then begin
-                    DEBUG('FIND "'+fndNAME +'" in file "'+Node.Get_Target_fullName+'"');
+                if (tool.CurPos.Flag=cafWord)and
+                   (tool.UpAtomIs(fndNAME))
+                then begin
+                    DEBUG('FIND "'+fndNAME +'" SP='+inttostr(tool.CurPos.StartPos)+' EP='+inttostr(tool.CurPos.EndPos)+' in file "'+Node.Get_Target_fullName+'"');
+                    result:=CodeToolBoss.SourceChangeCache.Replace(gtNone,gtNone,tool.CurPos.StartPos,tool.CurPos.EndPos,fndNAME);
+                    if result then inc(needSave)
+                    else begin
+                       _mssge_:='CodeToolBoss.SourceChangeCache.Replace: ERR';
+                        BREAK;
+                    end;
                 end;
                 Tool.ReadNextAtom;
             end;
-
-
-            {UsesNode:=Tool.FindMainUsesNode;
-
-            if Assigned(UsesNode) then begin
-                _mssge_:=_mssge_+'OKOKOKOK';
-                 result:=true;
-            end
-            else begin
-                _mssge_:=_mssge_+'main unit section NOT found in "'+_mssge_+'"';
-                 result:=true;
-            end;}
-
-
-
-
-
-            {result:=Tool.RenameUsedUnit(ExtractFileNameOnly(_src_.Get_Source_obj_Name), ExtractFileNameOnly(_src_.Get_Target_obj_Name),'',CodeToolBoss.SourceChangeCache);
-            if not result then begin
-               _mssge_:='Tool.RenameUsedUnit: ERR';
-            end;}
-
-
-
-
-
-
-
-
-
-
-
-
-              //if UsesSection=usMain then
-             { try Tool.BuildTree(lsrMainUsesSectionEnd)
-              except _mssge_:=' BuildTree FAIL ' end;
-              UsesNode:=Tool.FindMainUsesNode;
-
-              if Assigned(UsesNode) then begin
-                  _mssge_:=_mssge_+'OKOKOKOK';
-                   result:=true;
-              end
-              else begin
-                  _mssge_:=_mssge_+'main unit section NOT found in "'+_mssge_+'"';
-                   result:=true;
-              end; }
-
-
-              //else
-              //  BuildTree(lsrImplementationUsesSectionEnd);
-              //case UsesSection Of
-              //  usMain: UsesNode:=FindMainUsesNode;
-              //  usImplementation: UsesNode:=FindImplementationUsesNode;
-              //end;
-              //Result:=UnitExistsInUsesSection(UsesNode,AnUnitName);
-           // end;
-
-
         end;
-        //---
+
+        // Step 4: Apply the changes
+        if result and (needSave>0) then begin
+             result:=CodeToolBoss.SourceChangeCache.Apply;
+             if not result then _mssge_:='CodeToolBoss.SourceChangeCache.Apply:"'+_mssge_+'" ER'
+        end;
+        // Step 5: SAVE the changes
         if result then begin
             result:=code.Save;
             if not result then _mssge_:='code.Save:"'+_mssge_+'" ER'
         end;
         //----------
-        //if result then begin
-        //    _mssge_:='changes APPLIED: unit "'+{ExtractFileNameOnly(Node.Get_Source_obj_Name)+'" -> "'+ExtractFileNameOnly(Node.Get_Target_obj_Name)+}'" in file "'+Node.Get_Target_fullName+'"';
-        //end;
+        if result then begin
+            _mssge_:='changes APPLIED '+inttostr(needSave)+': unit "'+{ExtractFileNameOnly(Node.Get_Source_obj_Name)+'" -> "'+ExtractFileNameOnly(Node.Get_Target_obj_Name)+}'" in file "'+Node.Get_Target_fullName+'"';
+        end;
      end
      else begin
         _mssge_:='TARGET File:'+'"'+Node.Get_Target_fullName+'"'+' NOT exists';
